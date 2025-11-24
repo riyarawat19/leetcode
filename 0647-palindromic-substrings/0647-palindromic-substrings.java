9897dbcf5edc5bbce04560a9debb19d3b0d134b6
Riya Rawat <riyarawat.1904@gmail.com>
2025-11-24 12:50:10 +0530

    Time: 19 ms (27.76%), Space: 46.9 MB (11.77%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n=s.length();
+        
+         boolean[][]dp= new boolean[n][n];
+
+          
+           int count=0;
+
+         for(int len=1;len<=n;len++){
+            for(int i=0; i+len-1<n ;i++){
+                 int j= i+len-1;
+
+                  if(s.charAt(i)==s.charAt(j)){
+                     if(len<=2){         // 2 ab already rejected
+                         dp[i][j]= true;
+                     }
+
+                     else{
+                         dp[i][j]= dp[i+1][j-1];// in length more than 3  i and j pointer shrink j
+                     }
+                  }
+                   if(dp[i][j]) count++;
+            }
+         }
+          return count;
+    }
+}
