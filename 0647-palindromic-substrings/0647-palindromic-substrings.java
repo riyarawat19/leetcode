fb9d6cfd9250ecc46ae8fca12c9ad0db22b82afb
Riya Rawat <riyarawat.1904@gmail.com>
2025-11-24 12:56:47 +0530

    Time: 6 ms (66.28%), Space: 42.3 MB (56.23%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,28 +1,47 @@
-class Solution {
-    public int countSubstrings(String s) {
-        int n=s.length();
+// class Solution {
+//     public int countSubstrings(String s) {
+//         int n=s.length();
         
-         boolean[][]dp= new boolean[n][n];
+//          boolean[][]dp= new boolean[n][n];
 
           
-           int count=0;
+//            int count=0;
 
-         for(int len=1;len<=n;len++){
-            for(int i=0; i+len-1<n ;i++){
-                 int j= i+len-1;
+//          for(int len=1;len<=n;len++){
+//             for(int i=0; i+len-1<n ;i++){
+//                  int j= i+len-1;
 
-                  if(s.charAt(i)==s.charAt(j)){
-                     if(len<=2){         // 2 ab already rejected
-                         dp[i][j]= true;
-                     }
+//                   if(s.charAt(i)==s.charAt(j)){
+//                      if(len<=2){         // 2 ab already rejected
+//                          dp[i][j]= true;
+//                      }
 
-                     else{
-                         dp[i][j]= dp[i+1][j-1];// in length more than 3  i and j pointer shrink j
-                     }
-                  }
-                   if(dp[i][j]) count++;
+//                      else{
+//                          dp[i][j]= dp[i+1][j-1];// in length more than 3  i and j pointer shrink j
+//                      }
+//                   }
+//                    if(dp[i][j]) count++;
+//             }
+//          }
+//           return count;
+//     }
+// }
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+
+        for (int center = 0; center < 2 * n - 1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                count++;
+                left--;
+                right++;
             }
-         }
-          return count;
+        }
+
+        return count;
     }
 }
